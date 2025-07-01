f1cc0e081224a1941637cffde260757848ab2f15
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 00:04:03 +0200

    feature: add loadcontent (#35)

--- packages/references-view/src/parts/LoadContent/LoadContent.ts
+++ packages/references-view/src/parts/LoadContent/LoadContent.ts
@@ -1,7 +1,19 @@
 import type { ReferencesState } from '../ReferencesState/ReferencesState.ts'
+import { getDisplayReferences } from '../GetDisplayReferences/GetDisplayReferences.ts'
+import { getFileCount } from '../GetReferencesFileCount/GetReferencesFileCount.ts'
+import { getMessage } from '../GetReferencesMessage/GetReferencesMessage.ts'
+import { getReferences } from '../References/References.ts'
 
 export const loadContent = async (state: ReferencesState): Promise<ReferencesState> => {
+  const editor = {}
+  const references = await getReferences(editor)
+  const displayReferences = getDisplayReferences(references)
+  const fileCount = getFileCount(references)
+  const message = getMessage(references.length, fileCount)
   return {
     ...state,
+    references,
+    displayReferences,
+    message,
   }
 }
