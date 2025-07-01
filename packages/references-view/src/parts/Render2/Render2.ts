1d2471ce876742d69305ac3aac5a40d4ecdb86f8
Le Vivilet <levivlet@pm.me>
2025-07-01 19:01:51 +0200

    feature: update renderer

---
+++ packages/references-view/src/parts/Render2/Render2.ts
@@ -0,0 +1,10 @@
+import type { ViewletCommand } from '../ViewletCommand/ViewletCommand.ts'
+import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
+import * as RunAndDebugStates from '../ReferencesStates/ReferencesStates.ts'
+
+export const render2 = (uid: number, diffResult: readonly number[]): readonly ViewletCommand[] => {
+  const { oldState, newState } = RunAndDebugStates.get(uid)
+  RunAndDebugStates.set(uid, newState, newState)
+  const commands = ApplyRender.applyRender(oldState, newState, diffResult)
+  return commands
+}
