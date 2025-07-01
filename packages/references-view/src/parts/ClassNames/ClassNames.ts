3673d7eda60ccd24e40d20e6c37ca5784e901fb2
Le Vivilet <levivlet@pm.me>
2025-07-01 23:51:39 +0200

    feature: update classnames

--- packages/references-view/src/parts/ClassNames/ClassNames.ts
+++ packages/references-view/src/parts/ClassNames/ClassNames.ts
@@ -1,37 +1,6 @@
-import { ClassNames } from '@lvce-editor/virtual-dom-worker'
-
-export const {
-  CallStackArrow,
-  CallStackDescription,
-  CallStackLabel,
-  Chevron,
-  DebugButton,
-  DebugButtons,
-  DebugPausedMessage,
-  DebugPropertyChevron,
-  DebugPropertyKey,
-  DebugRow,
-  DebugRowCallStack,
-  DebugRowCheckBox,
-  DebugRowInputField,
-  DebugSectionAction,
-  DebugSectionActions,
-  DebugSectionHeader,
-  DebugValue,
-  DebugValueBoolean,
-  DebugValueFunction,
-  DebugValueGetter,
-  DebugValueNumber,
-  DebugValueObject,
-  DebugValueScopeName,
-  DebugValueString,
-  DebugValueSymbol,
-  DebugValueUndefined,
-  DeleteWatchExpression,
-  IconButton,
-  InputBox,
-  InputLabel,
-} = ClassNames
-
-export const DebugRowSelected = 'DebugRowSelected'
-export const DebugToken = 'DebugToken'
+export const FileIcon = 'FileIcon'
+export const Highlight = 'Highlight'
+export const Label = 'Label'
+export const LocationList = 'LocationList'
+export const LocationsMessage = 'LocationsMessage'
+export const TreeItem = 'TreeItem'
