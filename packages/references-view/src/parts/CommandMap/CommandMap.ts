89341dfc9af29e70e733416f749b68b0e90996c3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-01 19:05:22 +0200

    feature: update commandmap (#11)

--- packages/references-view/src/parts/CommandMap/CommandMap.ts
+++ packages/references-view/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,18 @@
+import * as Create from '../Create/Create.ts'
+import * as Diff2 from '../Diff2/Diff2.ts'
+import * as Dispose from '../Dispose/Dispose.ts'
+import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
+import * as LoadContent from '../LoadContent/LoadContent.ts'
+import * as WrapCommand from '../ReferencesStates/ReferencesStates.ts'
+import * as Render2 from '../Render2/Render2.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
-  'RunAndDebug.terminate': Terminate.terminate,
+  'References.create': Create.create,
+  'References.diff2': Diff2.diff2,
+  'References.dispose': Dispose.dispose,
+  'References.getCommandIds': GetCommandIds.getCommandIds,
+  'References.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
+  'References.render2': Render2.render2,
+  'References.terminate': Terminate.terminate,
 }
