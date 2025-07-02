4559ead14b7e65a1905f44b3429dcf710a516058
Le Vivilet <levivlet@pm.me>
2025-07-02 13:29:34 +0200

    feature: add rpc tests

--- packages/references-view/test/InitializeExtensionHost.test.ts
+++ packages/references-view/test/InitializeExtensionHost.test.ts
@@ -1,6 +1,35 @@
-import { test, expect } from '@jest/globals'
-import * as InitializeExtensionHost from '../src/parts/InitializeExtensionHost/InitializeExtensionHost.js'
+import { expect, jest, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as ExtensionHost from '../src/parts/ExtensionHost/ExtensionHost.ts'
+import { initializeExtensionHost } from '../src/parts/InitializeExtensionHost/InitializeExtensionHost.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
-test('initializeExtensionHost should be a function', () => {
-  expect(typeof InitializeExtensionHost.initializeExtensionHost).toBe('function')
+test('initializeExtensionHost - success', async () => {
+  const mockInvokeRendererWorker = jest.fn()
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: () => {},
+    invokeAndTransfer: mockInvokeRendererWorker,
+  })
+  RendererWorker.set(mockRpc)
+  await initializeExtensionHost()
+  await ExtensionHost.dispose()
+  expect(mockInvokeRendererWorker).toHaveBeenCalledTimes(1)
+  expect(mockInvokeRendererWorker).toHaveBeenCalledWith(
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
+    expect.anything(),
+    'HandleMessagePort.handleMessagePort2',
+    55,
+  )
+})
+
+test('initializeExtensionHost - error', async () => {
+  const mockInvokeRendererWorker = jest.fn().mockRejectedValue(new Error('test error') as never)
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: () => {},
+    invokeAndTransfer: mockInvokeRendererWorker,
+  })
+  RendererWorker.set(mockRpc)
+  await expect(initializeExtensionHost()).rejects.toThrow(new Error('Failed to create extension host rpc: test error'))
 })
