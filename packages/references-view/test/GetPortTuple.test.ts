fe72a0a8cf6abe38630b5c0d92c51c7c69fbc655
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 13:46:37 +0200

    feature: remove unused port code (#61)

--- packages/references-view/test/GetPortTuple.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@jest/globals'
-import * as GetPortTuple from '../src/parts/GetPortTuple/GetPortTuple.js'
-
-test('getPortTuple should return an object with port1 and port2', () => {
-  const result = GetPortTuple.getPortTuple()
-  expect(result).toHaveProperty('port1')
-  expect(result).toHaveProperty('port2')
-})
