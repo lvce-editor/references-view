f497c5f2030183ed31648c66f8f17b59e8e321a4
Le Vivilet <levivlet@pm.me>
2025-07-02 12:46:00 +0200

    feature: add dispose test

---
+++ packages/references-view/test/Dispose.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@jest/globals'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import { dispose } from '../src/parts/Dispose/Dispose.ts'
+import * as ReferencesStates from '../src/parts/ReferencesStates/ReferencesStates.ts'
+
+test('dispose', async () => {
+  const uid = 123
+  const state = createDefaultState()
+  ReferencesStates.set(uid, state, state)
+  await dispose(uid)
+  expect(ReferencesStates.get(uid)).toBeUndefined()
+})
