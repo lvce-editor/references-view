4d20b3442230afff92590351581594be9806de08
Le Vivilet <levivlet@pm.me>
2025-07-02 14:00:50 +0200

    linttsc

--- packages/references-view/test/CreateDefaultState.test.ts
+++ packages/references-view/test/CreateDefaultState.test.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@jest/globals'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.js'
-import * as ReferencesStates from '../src/parts/ReferencesStates/ReferencesStates.js'
 
 test('createDefaultState should create state with default values', () => {
   const state = CreateDefaultState.createDefaultState()
