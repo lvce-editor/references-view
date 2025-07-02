ecb2c036040b43713fd54d6c93b59b8d36f743ed
Le Vivilet <levivlet@pm.me>
2025-07-02 16:42:17 +0200

    feature: simplify terminate code

--- packages/references-view/src/parts/CommandMap/CommandMap.ts
+++ packages/references-view/src/parts/CommandMap/CommandMap.ts
@@ -1,3 +1,4 @@
+import { terminate } from '@lvce-editor/viewlet-registry'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
 import * as Dispose from '../Dispose/Dispose.ts'
@@ -6,7 +7,6 @@ import * as Initialize from '../Initialize/Initialize.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as WrapCommand from '../ReferencesStates/ReferencesStates.ts'
 import * as Render2 from '../Render2/Render2.ts'
-import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
   'References.create': Create.create,
@@ -15,6 +15,6 @@ export const commandMap = {
   'References.getCommandIds': GetCommandIds.getCommandIds,
   'References.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
   'References.render2': Render2.render2,
-  'References.terminate': Terminate.terminate,
+  'References.terminate': terminate,
   'References.initialize': Initialize.initialize,
 }
