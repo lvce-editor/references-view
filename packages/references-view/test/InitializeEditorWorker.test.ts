e8f2c4c5a36af568082b3ffe9c5155255f62bf49
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 13:30:23 +0200

    feature: add rpc tests (#55)

--- packages/references-view/test/InitializeEditorWorker.test.ts
+++ packages/references-view/test/InitializeEditorWorker.test.ts
@@ -1,10 +1,24 @@
-import { test, expect } from '@jest/globals'
-import * as InitializeEditorWorker from '../src/parts/InitializeEditorWorker/InitializeEditorWorker.js'
+import { expect, jest, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as EditorWorker from '../src/parts/EditorWorker/EditorWorker.ts'
+import { initializeEditorWorker } from '../src/parts/InitializeEditorWorker/InitializeEditorWorker.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
-test('initializeEditorWorker should be a function', () => {
-  expect(typeof InitializeEditorWorker.initializeEditorWorker).toBe('function')
-})
-
-test('initializeEditorWorker should not throw when called', async () => {
-  await expect(InitializeEditorWorker.initializeEditorWorker()).resolves.not.toThrow()
+test('initializEditorWorker - success', async () => {
+  const mockInvokeRendererWorker = jest.fn()
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: () => {},
+    invokeAndTransfer: mockInvokeRendererWorker,
+  })
+  RendererWorker.set(mockRpc)
+  await initializeEditorWorker()
+  await EditorWorker.dispose()
+  expect(mockInvokeRendererWorker).toHaveBeenCalledTimes(1)
+  expect(mockInvokeRendererWorker).toHaveBeenCalledWith(
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker',
+    expect.anything(),
+    'HandleMessagePort.handleMessagePort',
+    55,
+  )
 })
