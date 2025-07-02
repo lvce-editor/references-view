57fbfe7b62ab36a4d28e0e188255e9e7896f2cdb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 14:01:44 +0200

    feature: simplify defaultstate (#63)

--- packages/references-view/test/CreateDefaultState.test.ts
+++ packages/references-view/test/CreateDefaultState.test.ts
@@ -1,6 +1,5 @@
 import { test, expect } from '@jest/globals'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.js'
-import * as ReferencesStates from '../src/parts/ReferencesStates/ReferencesStates.js'
 
 test('createDefaultState should create state with default values', () => {
   const state = CreateDefaultState.createDefaultState()
@@ -20,11 +19,4 @@ test('createDefaultState should create state with custom uid', () => {
   expect(state.references).toEqual([])
   expect(state.displayReferences).toEqual([])
   expect(state.focusedIndex).toBe(-1)
-})
-
-test('createDefaultState should register state in ReferencesStates', () => {
-  const state = CreateDefaultState.createDefaultState(456)
-  const retrievedState = ReferencesStates.get(456)
-
-  expect(retrievedState.newState).toBe(state)
 })
