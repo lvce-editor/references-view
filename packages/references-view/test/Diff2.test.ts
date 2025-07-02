7020bc9bb77b0b723ff8c8ed8584ecbf7a869d56
Le Vivilet <levivlet@pm.me>
2025-07-02 13:52:09 +0200

    feature: add diff test

---
+++ packages/references-view/test/Diff2.test.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@jest/globals'
+import * as Diff2 from '../src/parts/Diff2/Diff2.ts'
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as ReferencesStates from '../src/parts/ReferencesStates/ReferencesStates.ts'
+import * as DiffType from '../src/parts/DiffType/DiffType.ts'
+
+test('diff2 returns empty array when states are equal', () => {
+  const uid = 1
+  const state = createDefaultState(uid)
+
+  const result = Diff2.diff2(uid)
+
+  expect(result).toEqual([])
+})
+
+test('diff2 returns diff type when states are different', () => {
+  const uid = 2
+  const oldState = createDefaultState(uid)
+  const newState = {
+    ...oldState,
+    references: [{ type: 'leaf', index: 0, lineText: 'test' }]
+  }
+  ReferencesStates.set(uid, oldState, newState)
+
+  const result = Diff2.diff2(uid)
+
+  expect(result).toEqual([DiffType.RenderItems])
+})
+
+test('diff2 uses states from ReferencesStates', () => {
+  const uid = 3
+  const oldState = createDefaultState(uid)
+  const newState = {
+    ...oldState,
+    message: 'Different message',
+    references: [{ type: 'expanded', index: 1, name: 'test.ts' }]
+  }
+  ReferencesStates.set(uid, oldState, newState)
+
+  const result = Diff2.diff2(uid)
+
+  expect(result).toEqual([DiffType.RenderItems])
+})
