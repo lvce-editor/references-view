b88066a3abc672cd04f9f956f54ac0324f78fbfc
Le Vivilet <levivlet@pm.me>
2025-07-03 16:22:49 +0200

    feature: remove unused code

--- packages/references-view/src/parts/GetReferences/GetReferences.ts
+++
@@ -1,7 +0,0 @@
-import * as GetActiveEditor from '../GetActiveEditor/GetActiveEditor.js'
-import * as GetReferencesWithPreview from '../GetReferencesWithPreview/GetReferencesWithPreview.js'
-
-export const getReferences = async (): Promise<readonly any[]> => {
-  const editor = GetActiveEditor.getActiveEditor()
-  return GetReferencesWithPreview.getReferencesWithPreview(editor)
-}
