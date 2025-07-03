9dadfde110f1d63e6666db4280e3fe7bac7c4e00
Le Vivilet <levivlet@pm.me>
2025-07-03 13:10:28 +0200

    feature: add saveState function

---
+++ packages/references-view/src/parts/SaveState/SaveState.ts
@@ -0,0 +1,10 @@
+import type { SavedState } from '../SavedState/SavedState.ts'
+import * as r from '../ReferencesStates/ReferencesStates.ts'
+
+export const saveState = (id: number): SavedState => {
+  const { newState } = r.get(id)
+  const { message } = newState
+  return {
+    message,
+  }
+}
