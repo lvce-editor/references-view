8350184720b572a9357c0e6adae9d720f8d31ab0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-03 13:25:43 +0200

    feature: update diff (#81)

--- packages/references-view/src/parts/DiffItems/DiffItems.ts
+++ packages/references-view/src/parts/DiffItems/DiffItems.ts
@@ -4,5 +4,5 @@ import * as DiffType from '../DiffType/DiffType.ts'
 export const diffType = DiffType.RenderItems
 
 export const isEqual = (oldState: ReferencesState, newState: ReferencesState): boolean => {
-  return oldState.references === newState.references
+  return oldState.references === newState.references && oldState.displayReferences === newState.displayReferences && oldState.message === newState.message
 }
