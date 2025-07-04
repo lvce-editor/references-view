33270c01e1935a52f5954ab2baab2a5ceaed4b52
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 20:35:07 +0200

    feature: add keybindings (#112)

--- packages/references-view/src/parts/CommandMap/CommandMap.ts
+++ packages/references-view/src/parts/CommandMap/CommandMap.ts
@@ -8,6 +8,7 @@ import * as FocusLast from '../FocusLast/FocusLast.ts'
 import * as FocusNext from '../FocusNext/FocusNext.ts'
 import * as FocusPrevious from '../FocusPrevious/FocusPrevious.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
+import * as GetKeyBindings from '../GetKeyBindings/GetKeyBindings.ts'
 import * as HandleClickReference from '../HandleClickReference/HandleClickReference.ts'
 import * as Initialize from '../Initialize/Initialize.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
@@ -28,13 +29,14 @@ export const commandMap = {
   'References.focusNext': WrapCommand.wrapCommand(FocusNext.focusNext),
   'References.focusPrevious': WrapCommand.wrapCommand(FocusPrevious.focusPrevious),
   'References.getCommandIds': GetCommandIds.getCommandIds,
+  'References.getKeyBindings': GetKeyBindings.getKeyBindings,
+  'References.handleClickReference': WrapCommand.wrapCommand(HandleClickReference.handleClickReference),
+  'References.initialize': Initialize.initialize,
   'References.loadContent': WrapCommand.wrapCommand(LoadContent.loadContent),
   'References.render2': Render2.render2,
-  'References.restoreState': RestoreState.restoreState,
-  'References.terminate': terminate,
-  'References.initialize': Initialize.initialize,
-  'References.saveState': SaveState.saveState,
   'References.renderEventListeners': RenderEventListeners.renderEventListeners,
+  'References.restoreState': RestoreState.restoreState,
+  'References.saveState': SaveState.saveState,
   'References.selectIndex': WrapCommand.wrapCommand(SelectIndex.selectIndex),
-  'References.handleClickReference': WrapCommand.wrapCommand(HandleClickReference.handleClickReference),
+  'References.terminate': terminate,
 }
