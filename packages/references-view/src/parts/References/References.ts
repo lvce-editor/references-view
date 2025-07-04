0009221bb488873147c583f0f3f5c3b75f01ecbe
Le Vivilet <levivlet@pm.me>
2025-07-04 20:01:04 +0200

    feature: add explicit return type

--- packages/references-view/src/parts/References/References.ts
+++ packages/references-view/src/parts/References/References.ts
@@ -1,8 +1,9 @@
+import type { Reference } from '../Reference/Reference.ts'
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 import * as ExtensionHostReference from '../ExtensionHostReference/ExtensionHostReference.ts'
 import * as GetReferencesWithPreview from '../GetReferencesWithPreview/GetReferencesWithPreview.ts'
 
-export const getReferences = async (editorId: number): Promise<readonly any[]> => {
+export const getReferences = async (editorId: number): Promise<readonly Reference[]> => {
   // @ts-ignore
   const offset = await EditorWorker.getOffsetAtCursor(editorId)
   // @ts-ignore
