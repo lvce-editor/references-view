f028d88336ba8a8c1e52ba603e0765d8d44966bf
Le Vivilet <levivlet@pm.me>
2025-07-04 20:14:47 +0200

    feature: add event listeners

---
+++ packages/references-view/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -0,0 +1,11 @@
+import type { DomEventListener } from '../DomEventListener/DomEventListener.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+
+export const renderEventListeners = (): readonly DomEventListener[] => {
+  return [
+    {
+      name: DomEventListenerFunctions.HandleClickReference,
+      params: ['handleClick', 'event.target.dataset.index'],
+    },
+  ]
+}
