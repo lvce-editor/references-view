c527356200cf119f8e195bde7a3262b3b6f1e63c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 18:16:50 +0200

    feature: add requestIcon function (#91)

---
+++ packages/references-view/src/parts/RequestFileIcons/RequestFileIcons.ts
@@ -0,0 +1,12 @@
+import type { IconRequest } from '../IconRequest/IconRequest.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import { toSimpleIconRequest } from '../ToSimpleIconRequest/ToSimpleIconRequest.ts'
+
+export const requestFileIcons = async (requests: readonly IconRequest[]): Promise<readonly string[]> => {
+  if (requests.length === 0) {
+    return []
+  }
+  const simpleRequests = requests.map(toSimpleIconRequest)
+  const icons = await RendererWorker.invoke('IconTheme.getIcons', simpleRequests)
+  return icons
+}
