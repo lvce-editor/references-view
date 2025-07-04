e1c962da4e5546f059164cc4b522c34ec4dcf95f
Le Vivilet <levivlet@pm.me>
2025-07-04 20:07:31 +0200

    lint

--- packages/references-view/test/SelectIndex.test.ts
+++ packages/references-view/test/SelectIndex.test.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
-import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as LocationType from '../src/parts/LocationType/LocationType.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import { selectIndex } from '../src/parts/SelectIndex/SelectIndex.ts'
 
 test('selectIndex should return same state when index is out of bounds', async () => {
--- packages/references-view/test/SelectIndexLead.test.ts
+++ packages/references-view/test/SelectIndexLead.test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
-import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import { selectIndexLead } from '../src/parts/SelectIndexLead/SelectIndexLead.ts'
 
 test('selectIndexLead should update focusedIndex', async () => {
