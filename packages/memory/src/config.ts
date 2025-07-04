e960accb9f9aa2d0776e697e5edd94537f9404bd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 20:09:07 +0200

    feature: add explicit return type (#105)

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const threshold = 390_000
+export const threshold = 400_000
 
 export const instantiations = 10000
 
--- packages/references-view/src/parts/References/References.ts
+++ packages/references-view/src/parts/References/References.ts
@@ -1,8 +1,9 @@
+import type { Reference } from '../Reference/Reference.ts'
 import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
 import * as ExtensionHostReference from '../ExtensionHostReference/ExtensionHostReference.ts'
 import * as GetReferencesWithPreview from '../GetReferencesWithPreview/GetReferencesWithPreview.ts'
 
-export const getReferences = async (editorId: number): Promise<readonly any[]> => {
+export const getReferences = async (editorId: number): Promise<readonly Reference[]> => {
   const offset = await EditorWorker.getOffsetAtCursor(editorId)
   const references = await ExtensionHostReference.executeReferenceProvider(editorId, offset)
   const withPreview = await GetReferencesWithPreview.getReferencesWithPreview(references)
