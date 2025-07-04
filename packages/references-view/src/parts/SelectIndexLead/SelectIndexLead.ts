c8cab2964a101faa2909501aaedb04d5712728ff
Le Vivilet <levivlet@pm.me>
2025-07-04 20:01:44 +0200

    test

--- packages/references-view/src/parts/SelectIndexLead/SelectIndexLead.ts
+++ packages/references-view/src/parts/SelectIndexLead/SelectIndexLead.ts
@@ -1,11 +1,11 @@
 import type { ReferencesState } from '../ReferencesState/ReferencesState.ts'
 import { getUri } from '../GetUri/GetUri.ts'
-// import { openUri } from '../RendererWorker/RendererWorker.ts'
+import { openUri } from '../RendererWorker/RendererWorker.ts'
 
 export const selectIndexLead = async (state: ReferencesState, index: number): Promise<ReferencesState> => {
   const uri = getUri(state, index)
   // TODO open file and jump to line
-  // await openUri(uri)
+  await openUri(uri)
   return {
     ...state,
     focusedIndex: index,
