e96a6d3e16adf3ee9f217f4d324357903e402b67
Le Vivilet <levivlet@pm.me>
2025-07-05 20:58:12 +0200

    handle error

--- packages/references-view/src/parts/LoadContent/LoadContent.ts
+++ packages/references-view/src/parts/LoadContent/LoadContent.ts
@@ -8,30 +8,38 @@ import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import { requestFileIcons } from '../RequestFileIcons/RequestFileIcons.ts'
 
 export const loadContent = async (state: ReferencesState): Promise<ReferencesState> => {
-  // TODO need to wait for editor
-  const editorId = await RendererWorker.getActiveEditorId()
-  if (editorId === -1) {
+  try {
+    // TODO need to wait for editor
+    const editorId = await RendererWorker.getActiveEditorId()
+    if (editorId === -1) {
+      return {
+        ...state,
+        message: 'No Editor found',
+      }
+    }
+    const uri = await EditorWorker.invoke('Editor.getUri', editorId)
+    const languageId = await EditorWorker.invoke('Editor.getLanguageId', editorId)
+    console.log({ uri })
+    const offset = await EditorWorker.getOffsetAtCursor(editorId)
+    const position = await EditorWorker.getPositionAtCursor(editorId)
+    // @ts-ignore
+    const references = await References.getReferences2(uri, languageId, offset, position)
+    const icons = await requestFileIcons(references)
+    const displayReferences = GetDisplayReferences.getDisplayReferences(references, icons)
+    const fileCount = GetReferencesFileCount.getFileCount(references)
+    const message = GetReferencesMessage.getMessage(references.length, fileCount)
     return {
       ...state,
-      message: 'No Editor found',
+      references,
+      displayReferences,
+      message,
+      offset,
     }
-  }
-  const uri = await EditorWorker.invoke('Editor.getUri', editorId)
-  const languageId = await EditorWorker.invoke('Editor.getLanguageId', editorId)
-  console.log({ uri })
-  const offset = await EditorWorker.getOffsetAtCursor(editorId)
-  const position = await EditorWorker.getPositionAtCursor(editorId)
-  // @ts-ignore
-  const references = await References.getReferences2(uri, languageId, offset, position)
-  const icons = await requestFileIcons(references)
-  const displayReferences = GetDisplayReferences.getDisplayReferences(references, icons)
-  const fileCount = GetReferencesFileCount.getFileCount(references)
-  const message = GetReferencesMessage.getMessage(references.length, fileCount)
-  return {
-    ...state,
-    references,
-    displayReferences,
-    message,
-    offset,
+  } catch (error) {
+    // TODO send error to error worker
+    return {
+      ...state,
+      message: `${error}`,
+    }
   }
 }
