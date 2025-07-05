5c940230db2c44b743614d0c3817b22907b58538
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-05 21:32:23 +0200

    Discard changes to packages/references-view/src/parts/LoadContent/LoadContent.ts

--- packages/references-view/src/parts/LoadContent/LoadContent.ts
+++ packages/references-view/src/parts/LoadContent/LoadContent.ts
@@ -1,19 +1,32 @@
 import type { ReferencesState } from '../ReferencesState/ReferencesState.ts'
-import { restoreState } from '../RestoreState/RestoreState.ts'
-import { getAndUpdateReferences, updateReferences } from '../UpdateReferences/UpdateReferences.ts'
+import * as EditorWorker from '../EditorWorker/EditorWorker.ts'
+import * as GetDisplayReferences from '../GetDisplayReferences/GetDisplayReferences.ts'
+import * as GetReferencesFileCount from '../GetReferencesFileCount/GetReferencesFileCount.ts'
+import * as GetReferencesMessage from '../GetReferencesMessage/GetReferencesMessage.ts'
+import * as References from '../References/References.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import { requestFileIcons } from '../RequestFileIcons/RequestFileIcons.ts'
 
-export const loadContent = async (state: ReferencesState, savedState: unknown): Promise<ReferencesState> => {
-  try {
-    const { uri, offset, languageId, position } = restoreState(savedState)
-    if (uri) {
-      return updateReferences(state, uri, languageId, offset, position)
-    }
-    return getAndUpdateReferences(state)
-  } catch (error) {
-    // TODO send error to error worker
+export const loadContent = async (state: ReferencesState): Promise<ReferencesState> => {
+  // TODO need to wait for editor
+  const editorId = await RendererWorker.getActiveEditorId()
+  if (editorId === -1) {
     return {
       ...state,
-      message: `${error}`,
+      message: 'No Editor found',
     }
+  }
+  const offset = await EditorWorker.getOffsetAtCursor(editorId)
+  const references = await References.getReferences(editorId, offset)
+  const icons = await requestFileIcons(references)
+  const displayReferences = GetDisplayReferences.getDisplayReferences(references, icons)
+  const fileCount = GetReferencesFileCount.getFileCount(references)
+  const message = GetReferencesMessage.getMessage(references.length, fileCount)
+  return {
+    ...state,
+    references,
+    displayReferences,
+    message,
+    offset,
   }
 }
