0357bae94faa99fe2c188f9a9113fae46c10c7da
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-06 16:54:05 +0200

    fix: static build (#137)

--- packages/server/src/postinstall.js
+++ packages/server/src/postinstall.js
@@ -1,4 +1,4 @@
-import { cp, readFile, readdir, writeFile } from 'node:fs/promises'
+import { readFile, readdir, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 
@@ -30,10 +30,9 @@ const content = await readFile(rendererWorkerMainPath, 'utf-8')
 
 const remoteUrl = getRemoteUrl(workerPath)
 if (!content.includes('// const referencesWorkerUrl = ')) {
-  await cp(rendererWorkerMainPath, rendererWorkerMainPath + '.original')
   const occurrence = `const referencesWorkerUrl = \`\${assetDir}/packages/references-view/dist/referencesViewWorkerMain.js\``
   const replacement = `// const referencesWorkerUrl = \`\${assetDir}/packages/references-view/dist/referencesViewWorkerMain.js\`
-  const referencesWorkerUrl = \`${remoteUrl}\``
+const referencesWorkerUrl = \`${remoteUrl}\``
 
   const newContent = content.replace(occurrence, replacement)
   await writeFile(rendererWorkerMainPath, newContent)
