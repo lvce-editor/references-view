1ac94d9389920ed506691115386707d670efa165
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-01 23:43:39 +0200

    feature: enable empty e2e test (#149)

--- packages/references-view/src/parts/LoadContent/LoadContent.ts
+++ packages/references-view/src/parts/LoadContent/LoadContent.ts
@@ -2,10 +2,14 @@ import type { ReferencesState } from '../ReferencesState/ReferencesState.ts'
 import { restoreState } from '../RestoreState/RestoreState.ts'
 import { getAndUpdateReferences, updateReferences } from '../UpdateReferences/UpdateReferences.ts'
 
+const isMemory = (uri: string): boolean => {
+  return uri.startsWith('memfs://')
+}
+
 export const loadContent = async (state: ReferencesState, savedState: unknown): Promise<ReferencesState> => {
   try {
     const { uri, offset, languageId, position } = restoreState(savedState)
-    if (uri) {
+    if (uri && !isMemory(uri)) {
       return updateReferences(state, uri, languageId, offset, position)
     }
     return getAndUpdateReferences(state)
