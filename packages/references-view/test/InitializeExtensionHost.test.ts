d5da88b168786df194451e767d6c4edcb652303c
Le Vivilet <levivlet@pm.me>
2025-09-01 21:41:27 +0200

    feature: simplify extension rpc test

--- packages/references-view/test/InitializeExtensionHost.test.ts
+++ packages/references-view/test/InitializeExtensionHost.test.ts
@@ -1,35 +1,25 @@
 import { expect, jest, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as ExtensionHost from '../src/parts/ExtensionHost/ExtensionHost.ts'
 import { initializeExtensionHost } from '../src/parts/InitializeExtensionHost/InitializeExtensionHost.ts'
-import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 
 test('initializeExtensionHost - success', async () => {
-  const mockInvokeRendererWorker = jest.fn()
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: () => {},
-    invokeAndTransfer: mockInvokeRendererWorker,
+  const mockRpc = RendererWorker.registerMockRpc({
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker'() {},
   })
-  RendererWorker.set(mockRpc)
   await initializeExtensionHost()
   await ExtensionHost.dispose()
-  expect(mockInvokeRendererWorker).toHaveBeenCalledTimes(1)
-  expect(mockInvokeRendererWorker).toHaveBeenCalledWith(
-    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
-    expect.anything(),
-    'HandleMessagePort.handleMessagePort2',
-    0,
-  )
+  expect(mockRpc.invocations).toEqual([
+    ['SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker', expect.anything(), 'HandleMessagePort.handleMessagePort2', 0],
+  ])
 })
 
 test('initializeExtensionHost - error', async () => {
-  const mockInvokeRendererWorker = jest.fn().mockRejectedValue(new Error('test error') as never)
-  const mockRpc = MockRpc.create({
-    commandMap: {},
-    invoke: () => {},
-    invokeAndTransfer: mockInvokeRendererWorker,
+  RendererWorker.registerMockRpc({
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker'() {
+      throw new Error('test error')
+    },
   })
-  RendererWorker.set(mockRpc)
   await expect(initializeExtensionHost()).rejects.toThrow(new Error('Failed to create extension host rpc: test error'))
 })
