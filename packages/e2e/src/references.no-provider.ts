e549dd9d325f7e04f161f6c6fa64d269527fa895
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-01 23:29:22 +0200

    feature: add no provider e2e test (#147)

---
+++ packages/e2e/src/references.no-provider.ts
@@ -0,0 +1,30 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'references.no-provider'
+
+export const test: Test = async ({ FileSystem, Main, Editor, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/add.xyz`,
+    `export const add = () => {}
+`,
+  )
+  await FileSystem.writeFile(
+    `${tmpDir}/test.xyz`,
+    `import { add } from './add.ts'
+
+add(1,2)
+`,
+  )
+  await Main.openUri(`${tmpDir}/test.xyz`)
+  await Editor.setCursor(1, 0)
+
+  // act
+  await Editor.findAllReferences()
+
+  // assert
+  const viewletLocations = Locator('.Viewlet.Error')
+  await expect(viewletLocations).toBeVisible()
+  await expect(viewletLocations).toHaveText(`Error: Cannot read properties of undefined (reading 'provideReferences2')`)
+}
