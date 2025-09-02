ff108083c0d01ff13809f353d80b4640b46a7b50
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-02 10:46:54 +0200

    feature: simplify command ids (#160)

--- packages/references-view/src/parts/Listen/Listen.ts
+++ packages/references-view/src/parts/Listen/Listen.ts
@@ -1,8 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { registerCommands } from '../ReferencesStates/ReferencesStates.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
+  registerCommands(CommandMap.commandMap)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
