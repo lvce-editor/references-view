98f0c9cd1be056052a277172002e1bdb30213d54
Le Vivilet <levivlet@pm.me>
2025-09-02 10:48:53 +0200

    feature: improve references clear function

--- packages/references-view/src/parts/Clear/Clear.ts
+++ packages/references-view/src/parts/Clear/Clear.ts
@@ -1,5 +1,10 @@
 import type { ReferencesState } from '../ReferencesState/ReferencesState.ts'
 
 export const clear = (state: ReferencesState): ReferencesState => {
-  return state
+  return {
+    ...state,
+    references: [],
+    displayReferences: [],
+    message: 'No Results',
+  }
 }
