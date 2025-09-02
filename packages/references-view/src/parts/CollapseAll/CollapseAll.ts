8ee92a58938d96131f308b1ec41dececacde9455
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-09-02 11:02:02 +0200

    feature: on collapse all, updated collapsed uris (#166)

--- packages/references-view/src/parts/CollapseAll/CollapseAll.ts
+++ packages/references-view/src/parts/CollapseAll/CollapseAll.ts
@@ -1,5 +1,15 @@
+import type { Reference } from '../Reference/Reference.ts'
 import type { ReferencesState } from '../ReferencesState/ReferencesState.ts'
 
+const getUri = (reference: Reference): string => {
+  return reference.uri
+}
+
 export const collapseAll = async (state: ReferencesState): Promise<ReferencesState> => {
-  return state
+  const { references } = state
+  const uris = references.map(getUri)
+  return {
+    ...state,
+    collapsedUris: uris,
+  }
 }
