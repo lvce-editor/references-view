03e92f197b131ea3505fa7aea13b257e96749c80
Le Vivilet <levivilet@pm.me>
2026-01-22 20:08:13 +0100

    feature: pass platform to activateByEvent

--- packages/references-view/src/parts/ExecuteProvider/ExecuteProvider.ts
+++ packages/references-view/src/parts/ExecuteProvider/ExecuteProvider.ts
@@ -1,8 +1,20 @@
 import * as ExtensionHost from '../ExtensionHost/ExtensionHost.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
-export const executeProvider = async ({ event, method, params }: { event: string; method: string; params: readonly any[] }): Promise<any> => {
-  await RendererWorker.activateByEvent(event)
+export const executeProvider = async ({
+  event,
+  method,
+  params,
+  platform,
+  assetDir,
+}: {
+  event: string
+  method: string
+  params: readonly any[]
+  platform: number
+  assetDir: string
+}): Promise<any> => {
+  await RendererWorker.activateByEvent(event, assetDir, platform)
   // @ts-ignore
   const result = await ExtensionHost.invoke(method, ...params)
   return result
