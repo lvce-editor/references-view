b9f687d00b12afa0915e16c183792d3e6556e87d
Le Vivilet <levivilet@pm.me>
2026-01-22 20:28:09 +0100

    feature: add incremental renderer (#179)

--- packages/references-view/src/parts/GetRenderer/GetRenderer.ts
+++ packages/references-view/src/parts/GetRenderer/GetRenderer.ts
@@ -1,10 +1,13 @@
 import type { ReferencesState } from '../ReferencesState/ReferencesState.ts'
 import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
+import { renderIncremental } from '../RenderIncremental/RenderIncremental.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
 
 export const getRenderer = (diffType: number): Renderer<ReferencesState> => {
   switch (diffType) {
+    case DiffType.RenderIncremental:
+      return renderIncremental
     case DiffType.RenderItems:
       return RenderItems.renderItems
     default:
