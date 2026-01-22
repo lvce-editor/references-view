01d6caa4b783dff6590e3b98871df78db2482540
Le Vivilet <levivilet@pm.me>
2026-01-22 20:09:38 +0100

    args

--- packages/references-view/src/parts/References/References.ts
+++ packages/references-view/src/parts/References/References.ts
@@ -2,14 +2,21 @@ import type { Reference } from '../Reference/Reference.ts'
 import * as ExtensionHostReference from '../ExtensionHostReference/ExtensionHostReference.ts'
 import * as GetReferencesWithPreview from '../GetReferencesWithPreview/GetReferencesWithPreview.ts'
 
-export const getReferences = async (editorId: number, offset: number): Promise<readonly Reference[]> => {
-  const references = await ExtensionHostReference.executeReferenceProvider(editorId, offset)
+export const getReferences = async (editorId: number, offset: number, assetDir: string, platform: number): Promise<readonly Reference[]> => {
+  const references = await ExtensionHostReference.executeReferenceProvider(editorId, offset, assetDir, platform)
   const withPreview = await GetReferencesWithPreview.getReferencesWithPreview(references)
   return withPreview
 }
 
-export const getReferences2 = async (uri: string, languageId: string, offset: number, position: any): Promise<readonly Reference[]> => {
-  const references = await ExtensionHostReference.executeReferenceProvider2(uri, languageId, offset, position)
+export const getReferences2 = async (
+  uri: string,
+  languageId: string,
+  offset: number,
+  position: any,
+  assetDir: string,
+  platform: number,
+): Promise<readonly Reference[]> => {
+  const references = await ExtensionHostReference.executeReferenceProvider2(uri, languageId, offset, position, assetDir, platform)
   const withPreview = await GetReferencesWithPreview.getReferencesWithPreview(references)
   return withPreview
 }
