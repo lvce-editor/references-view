ec5436ce4b69bb0687dddea617e462f16abc169b
Le Vivilet <levivilet@pm.me>
2026-08-01 21:54:30 +0000

    feature: update eslint dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedVirtualDom,
   {
@@ -55,4 +56,4 @@ export default [
       'virtual-dom/prefer-merge-class-names': 'off',
     },
   },
-]
+])
