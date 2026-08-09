18ffaa3ff806f2c3a4ba42ee0da60b4966dd8871
Le Vivilet <levivilet@pm.me>
2026-08-09 15:43:37 -0700

    Update ESLint dependencies (#203)

--- packages/references-view/test/SendMessagePortToEditorWorker.test.ts
+++ packages/references-view/test/SendMessagePortToEditorWorker.test.ts
@@ -1,17 +1,21 @@
-import { test } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
 import { MockRpc } from '@lvce-editor/rpc'
 import * as RendererWorker from '../src/parts/RendererWorker/RendererWorker.ts'
 import * as SendMessagePortToEditorWorker from '../src/parts/SendMessagePortToEditorWorker/SendMessagePortToEditorWorker.ts'
 
 test('sendMessagePortToEditorWorker sends port to editor worker', async () => {
+  const invoke = jest.fn()
+  const invokeAndTransfer = jest.fn()
   const mockRpc = MockRpc.create({
     invoke: (method: string): any => {
+      invoke(method)
       if (method === 'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker') {
         return
       }
       throw new Error(`unexpected method ${method}`)
     },
     invokeAndTransfer: (method: string, port: unknown, command: string, rpcId: unknown): any => {
+      invokeAndTransfer(method, port, command, rpcId)
       if (method === 'SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker') {
         return
       }
@@ -22,4 +26,8 @@ test('sendMessagePortToEditorWorker sends port to editor worker', async () => {
   // @ts-ignore
   const { port2 } = new MessageChannel()
   await SendMessagePortToEditorWorker.sendMessagePortToEditorWorker(port2, 0)
+
+  expect(invoke).toHaveBeenCalledTimes(0)
+  expect(invokeAndTransfer).toHaveBeenCalledTimes(1)
+  expect(invokeAndTransfer).toHaveBeenCalledWith('SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker', port2, 'HandleMessagePort.handleMessagePort', 0)
 })
